715a537b91016707ba0b7e8a9b59afbde63f3e1e
V <nandithebull@outlook.com>
2023-12-27 02:04:48 +0000

    add ip as default author is none set

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ def child_blips(blip):
 
 @app.route("/d/<slug>")
 def index(slug):
-    author = request.args.get("author")
+    author = request.args.get("author") or request.remote_addr
     blips = []
     with Session(engine) as session:
         stmt = select(Blip).where(Blip.slug == slug)
