1a18961cf47b71532a1eee2b8d13218b423ae94a
V <V@fbi.gov>
2024-01-22 07:42:39 +0000

    revert

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,18 @@
-FROM debian:12-slim
+FROM jetpackio/devbox:latest
 
+
+# Installing your devbox project
+
+WORKDIR /home/devbox
+RUN mkdir /home/devbox/data
+USER ${DEVBOX_USER}:${DEVBOX_USER}
+
+COPY --chown=${DEVBOX_USER}:${DEVBOX_USER} . .
+
+RUN devbox run install 
+RUN devbox run build
+
+
+RUN devbox shellenv --init-hook >> ~/.profile
+
+CMD devbox run server
--- app.py
+++ app.py
@@ -1,22 +1,22 @@
-from emmett import App, request, response
-from emmett.orm import Database, Model, Field, belongs_to, has_many
-from emmett.tools import service
 import json
-
-app = App(__name__)
-
-
-class Blip(Model):
-    content = Field.string()
-    author = Field.string()
-    belongs_to("Blip")
-    has_many({"blips": "Blip"})
+import random
+from sqlalchemy import create_engine, select
+from model import Blip, Base
+from sqlalchemy.orm import Session, joinedload
+from flask import Flask, request, jsonify, render_template, send_from_directory
+from flask_cors import CORS
+from flask_socketio import SocketIO, emit
 
 
-app.config.db.uri = "sqlite:///data/blips.sql"
-db = Database(app)
-db.define_models(Blip)
-app.pipeline = [db.pipe]
+app = Flask(__name__)
+CORS(app)
+app.config["SECRET_KEY"] = "secret!"
+
+socketio = SocketIO(app, cors_allowed_origins="*")
+
+
+engine = create_engine("sqlite:///data/blips.sql", echo=True)
+Base.metadata.create_all(engine)
 
 
 def child_blips(blip):
@@ -35,77 +35,99 @@ def child_blips(blip):
 @app.route("/d/<slug>")
 def index(slug):
     author = request.args.get("author") or request.access_route[0]
-    blips = Blip.where(lambda b: b.slug == slug)
-    if len(blips) == 0:
-        blip = Blip.create(author=author, content="edit me", slug=slug)
-        blips.append(blip)
-    new_blips = []
-    for blip in blips:
-        new_blips.append(child_blips(blip))
-    blips = json.dumps(new_blips)
-    return dict(author=author, blips=blips, slug=slug)
+    blips = []
+    with Session(engine) as session:
+        stmt = select(Blip).where(Blip.slug == slug)
+        blips = session.scalars(stmt).all()
+        if len(blips) == 0:
+            blip = Blip(author=author, content="edit me", slug=slug)
+            session.add_all([blip])
+            session.commit()
+        new_blips = []
+        for blip in blips:
+            new_blips.append(child_blips(blip))
+        blips = json.dumps(new_blips)
+    return render_template("index.html", author=author, blips=blips, slug=slug)
+
+
+@app.route("/assets/<path:path>")
+def send_js(path):
+    return send_from_directory("templates/assets", path)
 
 
 @app.route("/doc/<slug>")
-@service.json
-async def doc(slug):
+def doc(slug):
     author = request.args.get("author")
-    blips = Blip.where(lambda b: b.slug == slug)
-    if len(blips) == 0:
-        blip = Blip.create(author=author, content="edit me", slug=slug)
-    new_blips = []
-    for blip in blips:
-        new_blips.append(child_blips(blip))
-    data = new_blips
-    return {"data": data}
-
+    with Session(engine) as session:
+        stmt = select(Blip).where(Blip.slug == slug)
+        blips = session.scalars(stmt).all()
+        if len(blips) == 0:
+            blip = Blip(author=author, content="edit me", slug=slug)
+            session.add_all([blip])
+            session.commit()
+        new_blips = []
+        for blip in blips:
+            new_blips.append(child_blips(blip))
+        data = new_blips
+    return jsonify(data)
 
 # create new blip and add set slug to page
 @app.route("/new/<slug>", methods=["POST"])
 def new(slug):
+    print(f"WHAT SLUG {slug}")
     data = request.get_json()
-    new_blip = Blip.create(content=data["content"], author=data["author"], slug=slug)
-    new_blip_id = new_blip.id
+    with Session(engine) as session:
+        new_blip = Blip(content=data["content"], author=data["author"], slug=slug)
+        print(f"NEW_BLIP {new_blip}")
+        session.add(new_blip)
+        session.commit()
+        new_blip_id = new_blip.id
     return str(new_blip_id)
-
 
 # adds sub blips to a blip
 @app.route("/add/<id>", methods=["POST"])
 def add(id):
     data = request.get_json()
-    blip = Blip.where(lambda b: b.id == id).first()
-    new_blip = Blip.create(content=data["content"], author=data["author"], blips=[])
-    blip.blips.append(new_blip)
-    blip.save()
-    new_blip_id = new_blip.id
+    with Session(engine) as session:
+        stmt = select(Blip).where(Blip.id == id)
+        blip = session.scalars(stmt).first()
+        new_blip = Blip(content=data["content"], author=data["author"], blips=[])
+        blip.blips.append(new_blip)
+        session.commit()
+        new_blip_id = new_blip.id
     return str(new_blip_id)
 
 
 # delete blip by id
 @app.route("/remove/<id>", methods=["POST"])
 def remove(id):
-    blip = Blip.where(lambda b: b.id == id).first()
-    blip.delete()
+    with Session(engine) as session:
+        stmt = select(Blip).where(Blip.id == id)
+        blip = session.scalars(stmt).first()
+        session.delete(blip)
+        session.commit()
     return "OK"
 
 
 @app.route("/edit/<id>", methods=["POST"])
 def edit(id):
     data = request.get_json()
-    blip = Blip.where(lambda b: b.id == id).first()
-    blip.content = data["content"]
-    blip.save()
+    with Session(engine) as session:
+        stmt = select(Blip).where(Blip.id == id)
+        blip = session.scalars(stmt).first()
+        blip.content = data["content"]
+        session.commit()
     return "OK"
 
 
-# @socketio.on("content")
-# def handle_content(content):
-#     print("Got content", content)
-#     emit("content", "ok", broadcast=True)
+@socketio.on("content")
+def handle_content(content):
+    print("Got content", content)
+    emit("content", "ok", broadcast=True)
 
 
-# if __name__ == "__main__":
-#     socketio.run(app, allow_unsafe_werkzeug=True)
+if __name__ == "__main__":
+    socketio.run(app, allow_unsafe_werkzeug=True)
 
 
 def generate_username():
